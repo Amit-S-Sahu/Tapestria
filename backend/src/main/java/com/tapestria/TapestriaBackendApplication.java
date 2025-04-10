d842ea911458bf7d7188b2122117d90192f361f7
Amit00199 <Amit.123854@stu.upes.ac.in>
2025-04-10 08:05:38 +0530

    Folder structure and backend setup

---
+++ backend/src/main/java/com/tapestria/TapestriaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tapestria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapestriaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapestriaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tapestria/TapestriaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapestria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapestriaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
